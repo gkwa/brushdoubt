e3be0425b429fb9938cb55eeda575ac13d070405
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-04 13:49:40 -0800

    real men dont make git commit comments

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ls -l1d /etc/*
-sudo cat /etc/resolv.conf
-sudo touch /etc/resolv.conf
-echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
-
-yum -y install iputils
-ping -c 2 google.com
