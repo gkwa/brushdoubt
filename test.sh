68fd69daaec73215ac814a9312ee99aa628e6aee
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-04 12:39:28 -0800

    real men dont make git commit comments

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 yum -y install tar gzip curl
 curl --create-dirs -sSo /opt/sbx/InstallSbxCDI.tgz https://streambox-cdi.s3-us-west-2.amazonaws.com/main/linux/InstallSbxCDI.tgz
