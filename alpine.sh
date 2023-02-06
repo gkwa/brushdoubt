1e87c6c2f6b6fcca10450a0e9d60637f91f4dad7
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-05 17:22:11 -0800

    real men dont make git commit comments

--- alpine.sh
+++ alpine.sh
@@ -6,3 +6,9 @@ set -u
 
 apk add --update iputils
 apk add --update bind-tools
+apk add --update curl
+apk add --update tar
+apk add --update gzip
+apk add --update coreutils
+apk add --update bash
+
