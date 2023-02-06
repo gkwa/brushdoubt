ad52b99280d512122a93c033022fceb589a1e21d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-05 21:13:42 -0800

    refactor

---
+++ setup_runner.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+
+sudo apt-get update >/dev/null
+sudo snap install distrobuilder --classic >/dev/null
+sudo apt-get -y install lxc debootstrap squashfs-tools awscli >/dev/null
+lxd init --auto
+lxd init --dump
+lxc profile show default
+
+distrobuilder --version
+
+# local images
+lxc image list 
+
+# public images
+lxc image list images: 
+lxc profile show default
+
+ping -c 1 google.com
+
+sudo cat /etc/resolv.conf
